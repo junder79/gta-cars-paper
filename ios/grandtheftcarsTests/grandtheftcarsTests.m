b133d704133142ae7fafb30de08407478215040e
Nicolas Cisterna <nicolascisterna729@gmail.com>
2021-02-05 10:53:39 -0300

    se cambia nombre de aplicacion

--- ios/grandtheftcarsTests/grandtheftcarsTests.m
+++ ios/grandtheftcarsTests/grandtheftcarsTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface gtacarsTests : XCTestCase
+@interface grandtheftcarsTests : XCTestCase
 
 @end
 
-@implementation gtacarsTests
+@implementation grandtheftcarsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
