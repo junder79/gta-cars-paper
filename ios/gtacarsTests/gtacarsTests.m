e9a13ea374203907f72e24f24136cdb72b4ac697
Nicolas Cisterna <nicolascisterna729@gmail.com>
2021-04-09 23:19:20 -0300

    Revert "se cambia nombre de aplicacion"
    This reverts commit b133d704133142ae7fafb30de08407478215040e.

--- ios/gtacarsTests/gtacarsTests.m
+++ ios/gtacarsTests/gtacarsTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface grandtheftcarsTests : XCTestCase
+@interface gtacarsTests : XCTestCase
 
 @end
 
-@implementation grandtheftcarsTests
+@implementation gtacarsTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
